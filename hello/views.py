be5441a3d4a9ec90b5cc8e3f1a33a9e2513a89bd
Luka Guledani <monster.striker21@gmail.com>
2023-06-08 00:53:22 +0400

    my first training in python-django xD

--- hello/views.py
+++ hello/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def index(request):
-    return HttpResponse('Hello World') 
+def index(request, name):
+    return render(request, 'hello/index.html', {
+        'name': name.capitalize()
+    })
+
